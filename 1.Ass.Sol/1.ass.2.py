e9b81883fdda1909cfe872b53884a4168715f48d
sunbeam <kartiks40005@gmail.com>
2024-03-09 21:05:10 +0545

    Assign

--- 1.Ass.Sol/1.ass.2.py
+++
@@ -1,8 +0,0 @@
-#2] Write a Python Program to Convert Celsius To Fahrenheit vice versa.
-# fahrenheit = (celsius * 1.8) + 32
-faren=lambda cel:(cel * 1.8)+32
-cel=int(input("Enter the celcius: "))
-print(f"Celius to farenheit= {faren(cel)}")
-cel=lambda faren:(faren-32)*0.55
-faren=int(input("Enter the farenheit: "))
-print(f"Farenheit to celcius= {cel(faren)}")
--- 1.Ass.Sol/1.ass.6.py
+++
@@ -1,6 +0,0 @@
-def eligibilty():
-    age=int(input("Enter your age :  "))
-    if age>=18:
-        print(f"pesron with age {age} is eligible for vote")
-    else: print(f"pesron with age {age} is not eligible for vote")
-eligibilty()

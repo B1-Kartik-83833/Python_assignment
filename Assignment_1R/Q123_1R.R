0f3dc24f20c718073b3297e49393c0ca2ac96665
snbeam <kartiks40005@gmail.com>
2024-03-20 18:14:31 +0530

    assign

---
+++ Assignment_1R/Q123_1R.R
@@ -0,0 +1,19 @@
+#1)Write a R program to create a list containing strings, numbers, vectors and a logical values.
+list1=list(1,c("triber", "nano", "XUV"),"abc",T)
+print(list1[1])
+print(list1[2])
+print(list1[3])
+print(list1[4])
+#2)Write a R program to merge two given lists into one list.
+list2=list("a","b","c")
+list3=list(1,2,3)
+merge_list=c(list2,list3)
+print(merge_list)
+#3)Write a R program to add a new item c4 = "R Programing" to a given list.
+list4= list(c1 = 1:15, c2 = "C Programming" ,c3 = "HTML")
+list5=list(c4="R Programing")
+print(c(list4,list5))
+
+
+
+
